852b6f434d265f0e941fda04593897f27b310c7e
CodeModeYT <tillmann.menzer@outlook.com>
2024-06-02 21:03:58 +0200

    Fixes #1 [Prevents user from accessing game.html without game happening]

--- src/countdown.js
+++ src/countdown.js
@@ -13,8 +13,24 @@ var x = setInterval(function() {
   document.getElementById('cd_minutes').innerHTML = minutes;
   document.getElementById('cd_seconds').innerHTML = seconds;
 
-  if (distance < 0) {
+  if (distance <= 0) {
     clearInterval(x);
     window.location.replace("/game.html");
   }
 }, 1000);
+
+function checkTimeDistance(date){
+  const now = new Date();
+  const targetDate = new Date(date);
+  const diffInMillis = targetDate - now;
+  const diffInSeconds = Math.floor(diffInMillis / 1000);
+  return diffInSeconds;
+}
+
+// Check if the game already started --> if the user is allowed to see game.html
+function checkRedirect(date){
+  const distance = checkTimeDistance(date);
+  if (distance >= 0) {
+    window.location.replace("/index.html");
+  }
+}
