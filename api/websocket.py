15343609117c4ea4697ce12dc5cac6b172c85ef0
CodeModeYT <tillmann.menzer@outlook.com>
2024-06-04 22:19:00 +0200

    Updated to the next Germany match [GER-GR]

--- api/websocket.py
+++ api/websocket.py
@@ -5,9 +5,9 @@ from public.getMinute import get_current_minute
 
 async def notify(websocket, path):
     print("Client connected.")
-    match_id = 4450055
+    match_id = 4450056
     home_team = "Germany"
-    away_team = "Ukraine"
+    away_team = "Greece"
 
     try:
         monitor_task = asyncio.create_task(monitorScore(match_id, home_team, away_team, websocket))
